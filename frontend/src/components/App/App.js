e6f71d4dda9f1735f4cf781bb4467e8cf80fa4dd
Yuriy Pilipenko <yurik2061@gmail.com>
2023-08-17 10:32:21 +0300

    root

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,13 @@
+import { Container } from "../Container/Container";
+
+
+
+const App =() => {
+  return (
+<Container>
+<h1> react</h1>
+</Container>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Container/Container.styled.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+
+export const MainContainer = styled.div`
+    display: grid;
+    place-items: center;
+    /* justify-items: center; */
+    grid-gap: 25px;
+    background-color: var(--white-bg);
+    padding: 0 20px;
+    margin: 0 auto;
+    outline: 1px solid crimson;
+    width: 320px;
+
+      @media screen and (min-width: 768px) {
+      padding: 0 32px;
+      width: 768px;
+    }
+
+      @media screen and (min-width: 1280px) {
+        padding: 0 16px;
+        width: 1280px;
+    }
+`
