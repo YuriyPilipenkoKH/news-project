79f866854ee4e104336e0289f67b14114d60fb59
Yuriy Pilipenko <yurik2061@gmail.com>
2023-08-17 12:24:39 +0300

    gradient

--- frontend/src/components/Button/Button.styled.jsx
+++ frontend/src/components/Button/Button.styled.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 export const StyledButton = styled.button`
 
+font-family: inherit;
+font-weight: 600;
+height: 40px;
+
 display: flex;
 align-items: center;
 justify-content: center;
@@ -15,6 +19,16 @@ font-size: 16px;
 cursor: pointer;
 outline: none;
 
-transition: all 0.4s ease;
+transition: all 0.4s ease; 
+
+& >svg {
+  fill: var(--fone-color);
+  }
+
+  &:hover  {
+  transition: all 0.4s ease; 
+  background: linear-gradient(to right, #88c7fc, #4da5f3);
+
+ }
 
 `;
